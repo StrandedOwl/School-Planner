d00dcbfa1ecb8ed9644a768f884b5d45b2f426db
Lorenzo Hug <lorenzo.hug@espas.ch>
2024-10-24 13:13:30 +0200

    restructured database

--- School-Planner/Database/schema.sql
+++ School-Planner/Database/schema.sql
@@ -1,47 +1,41 @@
--- Tabelle für Lehrer
 CREATE TABLE teachers (
     teacher_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    vorname VARCHAR(50) NOT NULL,
-    nachname VARCHAR(50) NOT NULL,
-    fach_id INT NOT NULL
-    -- Der Fremdschlüssel zu subjects wird später hinzugefügt
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    subject_id INT NOT NULL
 );
 
--- Tabelle für Klassen
 CREATE TABLE classes (
-    klasse_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    klassen_name VARCHAR(50) NOT NULL,
-    lehrer_id INT NOT NULL,
-    FOREIGN KEY (lehrer_id) REFERENCES teachers(teacher_id)
+    class_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    class_name VARCHAR(50) NOT NULL,
+    teacher_id INT NOT NULL,
+    FOREIGN KEY (teacher_id) REFERENCES teachers(teacher_id)
 );
 
--- Tabelle für Schüler
 CREATE TABLE students (
     student_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    vorname VARCHAR(50) NOT NULL,
-    nachname VARCHAR(50) NOT NULL,
-    geburtsdatum DATE NOT NULL,
-    klasse_id INT NOT NULL,
-    FOREIGN KEY (klasse_id) REFERENCES classes(klasse_id)
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    birthdate DATE NOT NULL,
+    class_id INT NOT NULL,
+    FOREIGN KEY (class_id) REFERENCES classes(class_id)
 );
 
--- Tabelle für Fächer
 CREATE TABLE subjects (
-    fach_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    fach_name VARCHAR(50) NOT NULL,
-    klasse_id INT NOT NULL,
-    FOREIGN KEY (klasse_id) REFERENCES classes(klasse_id)
+    subject_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    subject_name VARCHAR(50) NOT NULL,
+    class_id INT NOT NULL,
+    FOREIGN KEY (class_id) REFERENCES classes(class_id)
 );
 
--- Tabelle für Noten
 CREATE TABLE grades (
-    note_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    grade_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     student_id INT NOT NULL,
-    fach_id INT NOT NULL,
+    subject_id INT NOT NULL,
     teacher_id INT NOT NULL,
-    note DECIMAL(3, 1) NOT NULL,
-    datum DATE NOT NULL,
+    grade DECIMAL(3, 1) NOT NULL,
+    received DATE NOT NULL,
     FOREIGN KEY (student_id) REFERENCES students(student_id),
-    FOREIGN KEY (fach_id) REFERENCES subjects(fach_id),
+    FOREIGN KEY (subject_id) REFERENCES subjects(subject_id),
     FOREIGN KEY (teacher_id) REFERENCES teachers(teacher_id)
 );
